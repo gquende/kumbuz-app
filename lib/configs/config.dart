ef1a5784ee5c8c6b10dd63b9d5a7603589eda4a2
gquende <gquende@hotmail.com>
2025-03-29 19:57:12 +0100

    🐛 Update
    Update

--- lib/configs/config.dart
+++ lib/configs/config.dart
@@ -8,17 +8,22 @@ class AppEnvironment {
 }
 
 class AppFiles {
-  static String done_animation = "assets/animations/done.json";
-  static String conversation_animation = "assets/animations/conversation.json";
-  static String loading_animation = "assets/animations/loading.json";
-  static String LOGO = "assets/images/logo.png";
-  static String LOGO2 = "assets/images/logo2.png";
-  static final String APP_LOADING = "assets/animations/app-loading.json";
-  static final Onboarding1 = "assets/images/money_control.svg";
+  static const String done_animation = "assets/animations/done.json";
+  static const String conversation_animation =
+      "assets/animations/conversation.json";
+
+  static const String loading_animation = "assets/animations/loading.json";
+  static const String LOGO = "assets/images/logo.png";
+
+  static const String LOGO2 = "assets/images/logo2.png";
+  static const String APP_LOADING = "assets/animations/app-loading.json";
+
+  static const Onboarding1 = "assets/images/money_control.svg";
 }
 
 class AppColors {
   static final backgroundColor = Colors.white;
+
   //Other Good Color
   static const primaryColor = Color(0xFF665ced);
   static final bgColor = Color(0xFFf5f5f5);
